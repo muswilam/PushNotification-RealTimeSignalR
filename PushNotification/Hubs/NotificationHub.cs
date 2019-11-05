84cf80ab8802d693cd6a7c437a0dc5337bc739a8
Sully <jackswilam@gmail.com>
2019-11-05 16:08:35 +0200

    Create custom server-side hub class

---
+++ PushNotification/Hubs/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PushNotification.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public static void Send()
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+            context.Clients.All.displayStatus();
+        }
+    }
+}
