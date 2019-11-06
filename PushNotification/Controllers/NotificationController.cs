868ffba13127fe89aca81a6f3b62c520d9c822b9
Sully <jackswilam@gmail.com>
2019-11-06 16:13:00 +0200

    CRUD operations using jQuery ajax for testing pushing notification for all clients

--- PushNotification/Controllers/NotificationController.cs
+++ PushNotification/Controllers/NotificationController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PushNotification.Services;
 
 namespace PushNotification.Controllers
 {
@@ -12,12 +13,66 @@ namespace PushNotification.Controllers
         // GET: Notification
         public ActionResult Index()
         {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+
             return View();
         }
 
         public JsonResult GetNotification()
         {
             return Json(NotificationService.GetNotification(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Notification notification)
+        {
+            NotificationsServiceDb notify = new NotificationsServiceDb();
+            var result = notify.SaveNotification(notification);
+
+            if (result)
+                return RedirectToAction("Index");
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            NotificationsServiceDb notify = new NotificationsServiceDb();
+
+            var notification = notify.GetNotification(id);
+            return PartialView(notification);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Notification notification)
+        {
+            NotificationsServiceDb notify = new NotificationsServiceDb();
+
+            var result = notify.UpdateNotification(notification);
+
+            if (result)
+                return RedirectToAction("Index");
+
+            return View(notification);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Notification notification)
+        {
+            NotificationsServiceDb notify = new NotificationsServiceDb();
+
+            var result = notify.DeleteNotification(notification);
+
+            if (result)
+                return RedirectToAction("Index");
+
+            return new HttpStatusCodeResult(500);
         }
     }
 }
---
+++ PushNotification/Services/NotificationsServiceDb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PushNotification.Models;
+
+namespace PushNotification.Services
+{
+    public class NotificationsServiceDb
+    {
+        public Notification GetNotification(int id)
+        {
+            using (var context = new RealTimeNotiicationCS())
+            {
+                return context.Notifications.Where(n => n.Id == id).First();
+            }
+        }
+
+        public bool SaveNotification(Notification notification)
+        {
+            using (var context = new RealTimeNotiicationCS())
+            {
+                context.Notifications.Add(notification);
+                return context.SaveChanges() > 0;
+            }
+        }
+
+        public bool UpdateNotification(Notification notification)
+        {
+            using (var context = new RealTimeNotiicationCS())
+            {
+                context.Entry(notification).State = System.Data.Entity.EntityState.Modified;
+                return context.SaveChanges() > 0;
+            }
+        }
+
+        public bool DeleteNotification(Notification notification)
+        {
+            using (var context = new RealTimeNotiicationCS())
+            {
+                context.Entry(notification).State = System.Data.Entity.EntityState.Deleted;
+                return context.SaveChanges() > 0;
+            }
+        }
+
+        
+    }
+}
